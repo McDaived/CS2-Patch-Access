74b8336fcbed17192224f3f99c242debedb46d20
Daived <mc.daived1996@gmail.com>
2023-08-12 04:06:46 +0300

    new update

--- CS2Downloader/CS2Patch.hpp
+++ CS2Downloader/CS2Patch.hpp
@@ -1,8 +1,9 @@
 #pragma once
+
 #define _CRT_SECURE_NO_WARNINGS
 
 namespace Patcher {
-	bool ReplaceBytes(const char* filename, const char* searchPattern, const char* replaceBytes);
+	bool ReplaceBytes(const char* filename, const char* searchPattern, const char* replaceBytes, size_t replaceLength);
 	bool PatchClient();
 	bool PatchServer();
 	void CleanPatchFiles();
--- CS2Downloader/global.hpp
+++ CS2Downloader/global.hpp
@@ -3,6 +3,6 @@
 #include <iostream>
 
 namespace Globals {
-	inline std::string currentVersion = "1.1";
+	inline std::string currentVersion = "1.3";
 	inline bool usesNoManifests = false;
 }
